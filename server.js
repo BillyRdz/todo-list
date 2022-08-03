2b9b277e0335ebca72694e4e0715de926f899c8a
Billy Rodriguez <billyrdz@gmail.com>
2022-08-02 21:35:02 -0500

    CRUD implemented

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+//Connect to Mongo
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-  console.log("connected to database");
+  console.log("Connected to db!");
 });
 
+// GET METHOD
 app.get("/", async (req, res) => {
   try {
     TodoTask.find({}, (err, tasks) => {
@@ -25,6 +27,55 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+//POST METHOD
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    console.log(todoTask);
+    res.redirect("/");
+  } catch (err) {
+    if (err) return res.status(500).send(err);
+    res.redirect("/");
+  }
 });
+
+//UPDATE METHOD
+app
+  .route("/edit/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render("edit.ejs", { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(
+      id,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+
+      (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect("/");
+      }
+    );
+  });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+  });
+});
+
+//Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
